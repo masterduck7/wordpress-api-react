993530c51ce69117f04d0097b43a795a8cb9b741
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2024-01-12 17:20:31 +0100

    feat(react): Setup wordpress connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
 function App() {
+	const [posts, setPosts] = useState([]);
+	const getPots = async () => {
+		const { data } = await axios.get(
+			process.env.REACT_APP_WORDPRESS_SITE + "wp-json/wp/v2/posts"
+		);
+		setPosts(data);
+	};
+	useEffect(() => {
+		getPots();
+	}, []);
+
 	return (
 		<div>
-			<p>Hello World</p>
+			{posts.map(function (post) {
+				console.log(post.content.rendered);
+				return (
+					<React.Fragment>
+						<h1>ID: {post.id}</h1>
+						<h1>{post.title.rendered}</h1>
+						<React.Fragment>
+							<div
+								dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+							/>
+						</React.Fragment>
+					</React.Fragment>
+				);
+			})}
 		</div>
 	);
 }
